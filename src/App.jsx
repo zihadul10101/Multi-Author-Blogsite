41c7280f967cdc490ea0dd2d69ed7e0f130d9a41
Zihadul Islam <zihadul10101@gmail.com>
2022-01-24 15:49:44 +0600

    file setup and scss implement

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from "./components/home/Home";
+function App() {
+  return (
+ <Router>
+
+<Switch>
+
+<Route path="/" component={Home} exact />
+
+
+</Switch>
+
+
+ </Router>
+  );
+}
+
+export default App;
---
+++ src/components/home/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1>Navbar</h1>
+
+            <div className="row">
+                <div className="col-2">
+                    <h1>home</h1>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
