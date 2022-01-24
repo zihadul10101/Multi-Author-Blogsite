685f3ecc2c727e5422320e727b30629c0c460885
Zihadul Islam <zihadul10101@gmail.com>
2022-01-24 22:05:03 +0600

    home, navbar and scss implement

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
-
+import {
+    Link
+} from "react-router-dom";
 const Navbar = () => {
     return (
-        <div>
-            <h1>Navbar</h1>
+        <div id="navbar" className="navbar">
+            <div className="container">
 
-            <div className="row">
-                <div className="col-2">
-                    <h1>home</h1>
+                <div className="row">
+
+                    <div className="col-4">
+                        <div className="image-menubar">
+                            <Link className="image" to="/" >
+<img src=""  alt="" />
+
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="col-8">
+
+                    </div>
+
                 </div>
+
+
             </div>
+
         </div>
     );
 };
