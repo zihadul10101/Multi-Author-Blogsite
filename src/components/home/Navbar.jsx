910cc71562a1c1ccd2260b1140cf1cf82a807e6b
Zihadul Islam <zihadul10101@gmail.com>
2022-01-24 23:56:57 +0600

    standed in navbar

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {
     Link
 } from "react-router-dom";
+import {FaFacebookSquare,FaTwitterSquare,FaYoutubeSquare,falinkingSquare, FaGithubSquare} from 'react-icons/fa'
 const Navbar = () => {
     return (
         <div id="navbar" className="navbar">
@@ -12,13 +13,38 @@ const Navbar = () => {
                     <div className="col-4">
                         <div className="image-menubar">
                             <Link className="image" to="/" >
-<img src=""  alt="" />
+                                <img src="https://i.ibb.co/8XQtd3P/blogbg.png" alt="" />
 
                             </Link>
                         </div>
                     </div>
                     <div className="col-8">
-
+                        <ul className="link-list">
+                            <li className="link-item">
+                                <Link to="/about">About</Link>
+                            </li>
+                            <li className="link-item">
+                                <Link to="/contact">Contact</Link>
+                            </li>
+                            <li className="link-item">
+                                <Link to="/policy">Policy</Link>
+                            </li>
+                            <li className="link-item">
+                                <Link to="/facebook"><span><FaFacebookSquare /></span></Link>
+                            </li>
+                            <li className="link-item">
+                                <Link to="/twitter"><span><FaTwitterSquare /></span></Link>
+                            </li>
+                            <li className="link-item">
+                                <Link to="/youtube"><span><FaYoutubeSquare /></span></Link>
+                            </li>
+                            <li className="link-item">
+                                <Link to="/linkend"><span><falinkingSquare /></span></Link>
+                            </li>
+                            <li className="link-item">
+                                <Link to="/github"><span><FaGithubSquare /></span></Link>
+                            </li>
+                        </ul>
                     </div>
 
                 </div>
