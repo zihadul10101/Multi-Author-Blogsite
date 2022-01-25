d884594501909ec467fb976367a2662dd5f082d7
Zihadul Islam <zihadul10101@gmail.com>
2022-01-25 21:49:29 +0600

    search component completed

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,6 +5,32 @@ const Home = () => {
     return (
         <div className="home">
             <Navbar />
+            {/* main content start */}
+            <div className="main-content">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-8">
+                            <h3>Articles</h3>
+                        </div>
+                        <div className="col-4">
+                            {/* search content start */}
+                            <div className="search-category-tag">
+                                <div className="search">
+                                    <h2>Search</h2>
+                                    <div className="form-group">
+                                        <input type="text" className="form-control" placeholder="search" />
+                                    </div>
+                                    <div className="form-group">
+                                        <button className="btn btn-block">Search</button>
+                                    </div>
+                                </div>
+
+                            </div>
+                                {/* search content end */}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
