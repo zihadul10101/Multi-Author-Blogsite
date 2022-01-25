e585ea436ffd98145e98df78be24288cb4010d23
Zihadul Islam <zihadul10101@gmail.com>
2022-01-25 20:33:03 +0600

    commite by navbar

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -2,26 +2,30 @@ import React from 'react';
 import {
     Link
 } from "react-router-dom";
-import {FaFacebookSquare,FaTwitterSquare,FaYoutubeSquare, FaGithubSquare} from 'react-icons/fa';
-import {BsLinkedin, BsList} from 'react-icons/bs';
+import { FaFacebookSquare, FaTwitterSquare, FaYoutubeSquare, FaGithubSquare } from 'react-icons/fa';
+import { BsLinkedin, BsList } from 'react-icons/bs';
 const Navbar = () => {
     return (
-        <div id="navbar" className="navbar">
-            <div className="container">
 
+        // navbar start
+        <div id="navbar" className="navbar">
+            {/*globle container */}
+            <div className="container">
+                {/* layout row */}
                 <div className="row">
-                <input type="checkbox" id="toggle" />
+                    <input type="checkbox" id="toggle" />
+                    {/* 12 from 4 start */}
                     <div className="col-4">
-                  
                         <div className="image-menubar">
                             <Link className="image" to="/" >
                                 <img src="https://i.ibb.co/8XQtd3P/blogbg.png" alt="" />
-
                             </Link>
-
                             <label className="menu_icon" htmlFor="toggle"><BsList /></label>
                         </div>
                     </div>
+                    {/* 12 from 4 end */}
+
+                    {/* 12 from 8 start */}
                     <div className="col-8">
                         <ul className="link-list toggle">
                             <li className="link-item">
@@ -33,32 +37,35 @@ const Navbar = () => {
                             <li className="link-item">
                                 <Link to="/policy">Policy</Link>
                             </li>
-                           <div className="socail-icon">
-                           <li className="link-item">
-                                <Link to="/facebook"><span><FaFacebookSquare /></span></Link>
-                            </li>
-                            <li className="link-item">
-                                <Link to="/twitter"><span><FaTwitterSquare /></span></Link>
-                            </li>
-                            <li className="link-item">
-                                <Link to="/youtube"><span><FaYoutubeSquare /></span></Link>
-                            </li>
-                            <li className="link-item">
-                                <Link to="/linkend"><span><BsLinkedin /></span></Link>
-                            </li>
-                            <li className="link-item">
-                                <Link to="/github"><span><FaGithubSquare /></span></Link>
-                            </li>
-                           </div>
+                            {/* socail icons implementation start*/}
+                            <div className="socail-icon">
+                                <li className="link-item">
+                                    <Link to="/facebook"><span><FaFacebookSquare /></span></Link>
+                                </li>
+                                <li className="link-item">
+                                    <Link to="/twitter"><span><FaTwitterSquare /></span></Link>
+                                </li>
+                                <li className="link-item">
+                                    <Link to="/youtube"><span><FaYoutubeSquare /></span></Link>
+                                </li>
+                                <li className="link-item">
+                                    <Link to="/linkend"><span><BsLinkedin /></span></Link>
+                                </li>
+                                <li className="link-item">
+                                    <Link to="/github"><span><FaGithubSquare /></span></Link>
+                                </li>
+                            </div>
+                            {/* socail icons implementation end*/}
                         </ul>
                     </div>
-
+                    {/* 12 from 8 end */}
                 </div>
 
 
             </div>
 
         </div>
+         // navbar end
     );
 };
 
