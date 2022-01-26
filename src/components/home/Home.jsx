1d5a631ec9ab53300007b68bac96b0a2d59232ba
Zihadul Islam <zihadul10101@gmail.com>
2022-01-26 15:47:54 +0600

    TAG SECTION Category section completed design

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FaChevronCircleRight, FaChevronRight } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import Navbar from './Navbar';
 import PopularArticle from './PopularArticle';
 
@@ -6,7 +8,7 @@ const Home = () => {
     return (
         <div className="home">
             <Navbar />
-            {/* main content start */}
+            {/* main section start */}
             <div className="main-content">
                 <div className="container">
                     <div className="row">
@@ -14,7 +16,7 @@ const Home = () => {
                             <h3>Articles</h3>
                         </div>
                         <div className="col-4">
-                            {/* search content start */}
+                            {/* search section start */}
                             <div className="search-category-tag">
                                 <div className="search">
                                     <h2>Search</h2>
@@ -27,28 +29,90 @@ const Home = () => {
                                 </div>
 
                             </div>
-                                {/* search content end */}
+                            {/* search section end */}
 
-                                {/* PopularArticle start */}
-                                <div className="popular-artical">
-                                    <div className="title">
-                                        <h3>popular article</h3>
-                                    </div>
-                                    <PopularArticle />
+                            {/* PopularArticle section start */}
+                            <div className="popular-artical">
+                                <div className="title">
+                                    <h3>popular article</h3>
                                 </div>
-                                 {/* popular article end */}
-                                 <div className="flow-facebook">
-                                 <div className="title">
-                                        <h3>FLOWING ME</h3>
+                                <PopularArticle />
+                            </div>
+                            {/* popular article section end */}
+
+                            {/* FLOWING section start */}
+                            <div className="flow-facebook">
+                                <div className="title">
+                                    <h3>FLOWING ME</h3>
+                                </div>
+                                <div className="image">
+                                    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FLocal-Courier-182877200475921%2F&tabs=timeline&width=340&height=148&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=2932989300353178" style={{ width: "340px", height: "145px", border: "none", overflow: "hidden", scrolling: "no", frameborder: "0", allowfullscreen: "true", allow: "autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" }}></iframe>
+                                </div>
+                            </div>
+                            {/* FLOWING section end */}
+
+                            {/* Category section start */}
+                            <div className="category">
+                                <div className="title">
+                                    <h3>Category</h3>
+                                </div>
+                                <ul className="cate-list">
+                                    <div className="cate-item">
+                                        <li><FaChevronRight />  <Link to="/">Algrodem</Link></li>
+                                        <span className="">(5)</span>
                                     </div>
-                                    <div className="image">
-                                        <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FLocal-Courier-182877200475921%2F&tabs=timeline&width=340&height=148&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=2932989300353178"style={{ width:"340px", height:"145px", border:"none",overflow:"hidden", scrolling:"no" ,frameborder:"0", allowfullscreen:"true", allow:"autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"}}></iframe>
+                                </ul>
+                                <ul className="cate-list">
+                                    <div className="cate-item">
+                                        <li><FaChevronRight />  <Link to="/">Algrodem</Link></li>
+                                        <span className="">(5)</span>
                                     </div>
-                                 </div>
+                                </ul>
+                                <ul className="cate-list">
+                                    <div className="cate-item">
+                                        <li><FaChevronRight />  <Link to="/">Algrodem</Link></li>
+                                        <span className="">(5)</span>
+                                    </div>
+                                </ul>
+                                <ul className="cate-list">
+                                    <div className="cate-item">
+                                        <li><FaChevronRight />  <Link to="/">Algrodem</Link></li>
+                                        <span className="">(5)</span>
+                                    </div>
+                                </ul>
+                            </div>
+                            {/* Category section end */}
+
+                            {/* TAG SECTION start */}
+                            <div className="tag">
+                                <div className="title">
+                                    <h3>popular tag</h3>
+                                </div>
+                                <ul>
+                                    <li>
+                                        <Link to="/">Program</Link>
+                                    </li>
+
+                                    <li>
+                                        <Link to="/">Program</Link>
+                                    </li>
+
+                                    <li>
+                                        <Link to="/">Program</Link>
+                                    </li>
+
+                                    <li>
+                                        <Link to="/">Program</Link>
+                                    </li>
+                                </ul>
+
+                            </div>
+                              {/* TAG SECTION end */}
                         </div>
                     </div>
                 </div>
             </div>
+             {/* main section end */}
         </div>
     );
 };
