318ad572f5206562fdc4eedb28e09d87375ef42e
Zihadul Islam <zihadul10101@gmail.com>
2022-01-26 21:58:01 +0600

    popular image added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaChevronCircleRight, FaChevronRight } from 'react-icons/fa';
+import {  FaChevronRight } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import Navbar from './Navbar';
 import PopularArticle from './PopularArticle';
--- src/components/home/PopularArticle.jsx
+++ src/components/home/PopularArticle.jsx
@@ -8,7 +8,7 @@ const PopularArticle = () => {
         {/* 1st Article */}
         <div className="row">
            <div className="col-4">
-               <Link to="/" className="image"><img src="https://i.ibb.co/bX8z6rh/101387021-252269149386257-7628973931973574656-n.jpg" alt="" /></Link>
+               <Link to="/" className="image"><img src="https://i.ibb.co/WpVNgJc/react-1.jpg" alt="" /></Link>
            </div>
           <div className="col-8">
           <div className="title-time">
@@ -23,7 +23,7 @@ const PopularArticle = () => {
 
         <div className="row">
            <div className="col-4">
-               <Link to="/" className="image"><img src="https://i.ibb.co/bX8z6rh/101387021-252269149386257-7628973931973574656-n.jpg" alt="" /></Link>
+               <Link to="/" className="image"><img src="https://i.ibb.co/vzq9F9f/js.jpg" alt="" /></Link>
            </div>
           <div className="col-8">
           <div className="title-time">
