a811f905bfe3ada2354a77640667812b8434dde2
Zihadul Islam <zihadul10101@gmail.com>
2022-04-12 11:33:54 +0600

    Comment section completed

--- src/components/home/Comment.jsx
+++ src/components/home/Comment.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { BsTrash } from 'react-icons/bs'
 const Comment = () => {
     return (
-        <div className="comments">
+       <>
+       
+       <div className="comments">
             <div className="main-reply-comment">
                 <div className="image-comment-time-name">
                     <img src="https://i.ibb.co/WpVNgJc/react-1.jpg" alt="" />
@@ -29,6 +31,166 @@ const Comment = () => {
                                 </button>
                             </div>
                         </div>
+                        <div className="replay-comment">
+                            <div className="replay-comment-image-name-time">
+                                <img src="https://i.ibb.co/WpVNgJc/react-1.jpg" alt="" />
+                                <div className="name-time-comment">
+                                    <div className="name-time">
+                                        <h4>Zihadul Islam</h4>
+                                        <span>10 days ago</span>
+                                    </div>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, obcaecati!</p>
+                                    <div className="replay-btn">
+                                        <button>Replay</button>
+                                    </div>
+                                    <div className="replay-box">
+                                        <div className="image-input">
+                                            <img src="https://i.ibb.co/WpVNgJc/react-1.jpg" alt="" />
+                                            <input type="text" required placeholder='add a public replay' />
+                                        </div>
+                                        <div className="replay-submit">
+                                            <button className="calcle">
+                                                Cancle
+                                            </button>
+                                            <button className="submite">
+                                                Submit
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="action">
+                                <BsTrash />
+                            </div>
+                        </div>
+                        <div className="replay-comment">
+                            <div className="replay-comment-image-name-time">
+                                <img src="https://i.ibb.co/WpVNgJc/react-1.jpg" alt="" />
+                                <div className="name-time-comment">
+                                    <div className="name-time">
+                                        <h4>Zihadul Islam</h4>
+                                        <span>10 days ago</span>
+                                    </div>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, obcaecati!</p>
+                                    <div className="replay-btn">
+                                        <button>Replay</button>
+                                    </div>
+                                    <div className="replay-box">
+                                        <div className="image-input">
+                                            <img src="https://i.ibb.co/WpVNgJc/react-1.jpg" alt="" />
+                                            <input type="text" required placeholder='add a public replay' />
+                                        </div>
+                                        <div className="replay-submit">
+                                            <button className="calcle">
+                                                Cancle
+                                            </button>
+                                            <button className="submite">
+                                                Submit
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="action">
+                                <BsTrash />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="action">
+                    <BsTrash />
+                </div>
+            </div>
+            <div className="main-reply-comment">
+                <div className="image-comment-time-name">
+                    <img src="https://i.ibb.co/WpVNgJc/react-1.jpg" alt="" />
+                    <div className="name-time-comment">
+                        <div className="name-time">
+                            <h4>Zihadul Islam</h4>
+                            <span>10 days ago</span>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, obcaecati!</p>
+                        <div className="replay-btn">
+                            <button>Replay</button>
+                        </div>
+                        <div className="replay-box">
+                            <div className="image-input">
+                                <img src="https://i.ibb.co/WpVNgJc/react-1.jpg" alt="" />
+                                <input type="text" required placeholder='add a public replay' />
+                            </div>
+                            <div className="replay-submit">
+                                <button className="calcle">
+                                    Cancle
+                                </button>
+                                <button className="submite">
+                                    Submit
+                                </button>
+                            </div>
+                        </div>
+                        <div className="replay-comment">
+                            <div className="replay-comment-image-name-time">
+                                <img src="https://i.ibb.co/WpVNgJc/react-1.jpg" alt="" />
+                                <div className="name-time-comment">
+                                    <div className="name-time">
+                                        <h4>Zihadul Islam</h4>
+                                        <span>10 days ago</span>
+                                    </div>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, obcaecati!</p>
+                                    <div className="replay-btn">
+                                        <button>Replay</button>
+                                    </div>
+                                    <div className="replay-box">
+                                        <div className="image-input">
+                                            <img src="https://i.ibb.co/WpVNgJc/react-1.jpg" alt="" />
+                                            <input type="text" required placeholder='add a public replay' />
+                                        </div>
+                                        <div className="replay-submit">
+                                            <button className="calcle">
+                                                Cancle
+                                            </button>
+                                            <button className="submite">
+                                                Submit
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="action">
+                                <BsTrash />
+                            </div>
+                        </div>
+                        <div className="replay-comment">
+                            <div className="replay-comment-image-name-time">
+                                <img src="https://i.ibb.co/WpVNgJc/react-1.jpg" alt="" />
+                                <div className="name-time-comment">
+                                    <div className="name-time">
+                                        <h4>Zihadul Islam</h4>
+                                        <span>10 days ago</span>
+                                    </div>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, obcaecati!</p>
+                                    <div className="replay-btn">
+                                        <button>Replay</button>
+                                    </div>
+                                    <div className="replay-box">
+                                        <div className="image-input">
+                                            <img src="https://i.ibb.co/WpVNgJc/react-1.jpg" alt="" />
+                                            <input type="text" required placeholder='add a public replay' />
+                                        </div>
+                                        <div className="replay-submit">
+                                            <button className="calcle">
+                                                Cancle
+                                            </button>
+                                            <button className="submite">
+                                                Submit
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="action">
+                                <BsTrash />
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="action">
@@ -36,6 +198,19 @@ const Comment = () => {
                 </div>
             </div>
         </div>
+        <div className="comment-submit">
+            <h2>Give Your Comment</h2>
+            <div className="form-group">
+                <textarea placeholder="Write Someting" className="form-control"  cols="20" rows="10"></textarea>
+
+            </div>
+            <div className="form-group">
+                <button className="btn">
+                    Submit
+                </button>
+            </div>
+        </div>
+       </>
     );
 };
 
