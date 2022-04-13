2fc77b1cfdb024115949bc5b8bb1e012c015818b
Zihadul Islam <zihadul10101@gmail.com>
2022-04-13 10:07:16 +0600

    UserMessage updated

--- src/components/dashbord/AdminInfo.jsx
+++ src/components/dashbord/AdminInfo.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const AdminInfo = () => {
     return (
-        <div>
-           <h1>Admin</h1> 
+        <div className="adminInfo">
+        <div className="image-email">
+        <img src="https://i.ibb.co/WpVNgJc/react-1.jpg" alt="" />
+        <span>Zihadul@gmail.com</span>
+        </div>
+        <ul>
+            <li>
+                <Link to='/dashborad/profile'>Profile</Link>
+            </li>
+        </ul>
         </div>
     );
 };
--- src/components/dashbord/UserMessage.jsx
+++ src/components/dashbord/UserMessage.jsx
@@ -1,13 +1,58 @@
 import React from 'react';
 import { BsEnvelope } from 'react-icons/bs';
+import { FaTrash } from 'react-icons/fa';
 const UserMessage = () => {
     return (
-        <div className='messages'>
+        <div className='message'>
             <div>
                 <span>
                     <BsEnvelope />
                     <div className="mCount">5</div>
                 </span>
+            </div>
+            <div className="messages show">
+                <ul>
+                    <li>
+                        <a href="#" target='_blank'>
+                            Zihadul send a Email
+                        </a>
+                        <div className="nDelete">
+                            <FaTrash />
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#" target='_blank'>
+                            Zihadul send a Email
+                        </a>
+                        <div className="nDelete">
+                            <FaTrash />
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#" target='_blank'>
+                            Zihadul send a Email
+                        </a>
+                        <div className="nDelete">
+                            <FaTrash />
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#" target='_blank'>
+                            Zihadul send a Email
+                        </a>
+                        <div className="nDelete">
+                            <FaTrash />
+                        </div>
+                    </li>
+                    <li>
+                        <a href="#" target='_blank'>
+                            Zihadul send a Email
+                        </a>
+                        <div className="nDelete">
+                            <FaTrash />
+                        </div>
+                    </li>
+                </ul>
             </div>
         </div>
     );
