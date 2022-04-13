e0d16a31631ef888f356b15a732876e216c9da56
Zihadul Islam <zihadul10101@gmail.com>
2022-04-13 10:26:48 +0600

    admin profile updated

--- src/components/dashbord/AdminInfo.jsx
+++ src/components/dashbord/AdminInfo.jsx
@@ -4,15 +4,22 @@ import { Link } from 'react-router-dom';
 const AdminInfo = () => {
     return (
         <div className="adminInfo">
-        <div className="image-email">
-        <img src="https://i.ibb.co/WpVNgJc/react-1.jpg" alt="" />
-        <span>Zihadul@gmail.com</span>
-        </div>
-        <ul>
-            <li>
-                <Link to='/dashborad/profile'>Profile</Link>
-            </li>
-        </ul>
+            <div className="image-email">
+                <img src="https://i.ibb.co/WpVNgJc/react-1.jpg" alt="" />
+                <span>Zihadul@gmail.com</span>
+            </div>
+            <ul>
+                <li>
+                    <Link to='/dashborad/profile'>Profile</Link>
+                </li>
+                <li>
+                    <Link to='/'>View site</Link>
+                </li>
+                <li>
+                    LogOut
+                </li>
+
+            </ul>
         </div>
     );
 };
