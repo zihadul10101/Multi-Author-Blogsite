ea3c49a6c834ef64bc2f2d638fe3198a5892de7c
Zihadul Islam <zihadul10101@gmail.com>
2022-04-18 00:08:52 +0600

    deashborad all artical nevber updated

--- src/components/dashbord/Dashborad.jsx
+++ src/components/dashbord/Dashborad.jsx
@@ -4,9 +4,15 @@ import Sidebar from './Sidebar';
 import {Switch,Route} from 'react-router-dom';
 import DashboradIndex from './DashboradIndex';
 import DashboradArtical from './DashboradArtical';
+import { Helmet } from 'react-helmet';
 const Dashborad = () => {
     return (
         <div className="dashborad">
+                <Helmet >
+              <title>
+                  Dashborad
+              </title>
+          </Helmet>
             <DashboradNavbar />
             <div className="dashborad-main-content">
                 <Sidebar />
--- src/components/dashbord/DashboradArtical.jsx
+++ src/components/dashbord/DashboradArtical.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
-
+import Helmet from 'react-helmet';
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 const DashboradArtical = () => {
     return (
-        <div>
-            <h1>dashboradArtical</h1>
+        <div className="dashborad-artical">
+            <Helmet >
+                <title>
+                    All Article
+                </title>
+            </Helmet>
+            <div className="artical-action-pagination">
+                <div className="numOf-search-newAdd">
+                    <div className="numOf">
+                        <h2>Article(22)</h2>
+                    </div>
+                    <div className="searchOf">
+                        <div className="search">
+                            <input type="text" placeholder="search Artical" className="form-control" />
+                            <span>
+                                <FaSearch />
+                            </span>
+                        </div>
+
+                    </div>
+                    <div className="newAdd">
+                        <Link to='/dashborad/add-artical' className="btn">
+                            New Add
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
