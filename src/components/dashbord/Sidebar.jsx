b5984960b6306258b641a31ecf90e31c80c04b3b
Zihadul Islam <zihadul10101@gmail.com>
2022-04-17 16:02:08 +0600

    sidebar updated

--- src/components/dashbord/Sidebar.jsx
+++ src/components/dashbord/Sidebar.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { AiFillDashboard } from 'react-icons/ai'
 import { RiArticleLine } from 'react-icons/ri'
-import {FaRegCaretSquareRight,FaTag } from 'react-icons/fa'
-import {HiUserGroup} from 'react-icons/hi'
-import {BsChevronRight} from 'react-icons/bs'
+import { FaRegCaretSquareRight, FaTag, FaEye, FaPlusCircle } from 'react-icons/fa'
+import { HiUserGroup } from 'react-icons/hi'
+import { BsChevronRight } from 'react-icons/bs'
 const Sidebar = () => {
     return (
         <div className="dashborad-main-content-sidebar">
             <ul>
+            <input type="checkbox"  id="artical" />
+            <input type="checkbox"  id="category" />
+            <input type="checkbox"  id="tag" />
+            <input type="checkbox"  id="user" />
+
                 <li>
                     <Link to='/dashborad'>
                         <label htmlFor="">
@@ -30,9 +35,23 @@ const Sidebar = () => {
                             </span>
                             <span>Article</span>
                         </h3>
-                        <span className='right-icon'> <BsChevronRight /> </span>
+                        <span className='right-icon1'> <BsChevronRight /> </span>
                     </label>
+                    <div className="article-categoory">
+                        <Link to="/">
+                            <span>
+                                <FaEye />
+                            </span>
+                            <span>All Article</span>
+                        </Link>
+                        <Link>
+                            <span>
+                                <FaPlusCircle />
+                                <span>Add Article</span>
+                            </span>
+                        </Link>
 
+                    </div>
                 </li>
                 <li>
 
@@ -43,8 +62,23 @@ const Sidebar = () => {
                             </span>
                             <span> Category</span>
                         </h3>
-                        <span className='right-icon'> <BsChevronRight /> </span>
+                        <span className='right-icon2'> <BsChevronRight /> </span>
                     </label>
+                    <div className="categoory-categoory">
+                        <Link to="/">
+                            <span>
+                                <FaEye />
+                            </span>
+                            <span>All Category</span>
+                        </Link>
+                        <Link>
+                            <span>
+                                <FaPlusCircle />
+                                <span>Add categoory</span>
+                            </span>
+                        </Link>
+
+                    </div>
 
                 </li>
                 <li>
@@ -52,13 +86,27 @@ const Sidebar = () => {
                     <label htmlFor="tag">
                         <h3>
                             <span>
-                                <FaTag  />
+                                <FaTag />
                             </span>
                             <span> Tag</span>
                         </h3>
-                        <span className='right-icon'> <BsChevronRight /> </span>
+                        <span className='right-icon3'> <BsChevronRight /> </span>
                     </label>
+                    <div className="tag-categoory">
+                        <Link to="/">
+                            <span>
+                                <FaEye />
+                            </span>
+                            <span>All Tag</span>
+                        </Link>
+                        <Link>
+                            <span>
+                                <FaPlusCircle />
+                                <span>Add Tag</span>
+                            </span>
+                        </Link>
 
+                    </div>
                 </li>
                 <li>
 
@@ -69,11 +117,25 @@ const Sidebar = () => {
                             </span>
                             <span> User</span>
                         </h3>
-                        <span className='right-icon'> <BsChevronRight /> </span>
+                        <span className='right-icon4'> <BsChevronRight /> </span>
                     </label>
+                    <div className="user-categoory">
+                        <Link to="/">
+                            <span>
+                                <FaEye />
+                            </span>
+                            <span>All User</span>
+                        </Link>
+                        <Link>
+                            <span>
+                                <FaEye />
+                                <span>Add Sub Admin</span>
+                            </span>
+                        </Link>
 
+                    </div>
                 </li>
-                
+
             </ul>
         </div>
     );
