1bc798db81962ff44c205bc4040a7b430f37ee87
Zihadul Islam <zihadul10101@gmail.com>
2022-04-21 00:01:41 +0600

    all sub admin updated

--- src/components/dashbord/AllSubAdmin.jsx
+++ src/components/dashbord/AllSubAdmin.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import {  FaSearch } from 'react-icons/fa';
+import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import Helmet from 'react-helmet';
 const AllSubAdmin = () => {
+    const user = 'admin';
+    const status = 'block';
     return (
         <div className="all-sub-admin">
-             <Helmet >
+            <Helmet >
                 <title>
                     All Sub Admin
                 </title>
             </Helmet>
             <div className="elements-numberOf-search-add_new">
-            <div className="numOf-search-newAdd">
+                <div className="numOf-search-newAdd">
                     <div className="numOf">
                         <h2>   All Sub Admin(22)</h2>
                     </div>
@@ -25,9 +27,111 @@ const AllSubAdmin = () => {
 
                     </div>
                     <div className="newAdd">
-                        <Link to='/dashborad/category-add' className="btn">
-                            Add New
+                        <Link to='/dashborad/all-user' className="btn">
+                            User
                         </Link>
+                    </div>
+                </div>
+                <div className="loading-elements">
+                    <div className="table-wapper">
+                        <table>
+                            <thead>
+                                <tr className="tr">
+                                    <th>No</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Image</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                               <tr>
+                               <td data-label='No'>1</td>
+                                <td data-label='Name'>Zihadul Islam</td>
+                                <td data-label='Email'>Zihadul1010@gmail.com</td>
+                                <td data-label='image' className='image'>
+                                    <img src="https://i.ibb.co/WpVNgJc/react-1.jpg" alt="" />
+
+                                </td>
+                                <td data-label='Action'>
+                                    {
+                                        user === 'admin' ? status === 'block' ? <span class="ussus">UnBlock</span> : <span class="sus">Block</span> : ''
+                                    }
+                                    <Link to='/dashborad/sub-admin/profile/55'>
+                                    Profile
+                                    </Link>
+                                </td>
+                               </tr>
+                               <tr>
+                               <td data-label='No'>1</td>
+                                <td data-label='Name'>Zihadul Islam</td>
+                                <td data-label='Email'>Zihadul1010@gmail.com</td>
+                                <td data-label='image' className='image'>
+                                    <img src="https://i.ibb.co/WpVNgJc/react-1.jpg" alt="" />
+
+                                </td>
+                                <td data-label='Action'>
+                                    {
+                                        user === 'admin' ? status === 'block' ? <span class="ussus">UnBlock</span> : <span class="sus">Block</span> : ''
+                                    }
+                                    <Link to='/dashborad/sub-admin/profile/55'>
+                                    Profile
+                                    </Link>
+                                </td>
+                               </tr>
+                               <tr>
+                               <td data-label='No'>1</td>
+                                <td data-label='Name'>Zihadul Islam</td>
+                                <td data-label='Email'>Zihadul1010@gmail.com</td>
+                                <td data-label='image' className='image'>
+                                    <img src="https://i.ibb.co/WpVNgJc/react-1.jpg" alt="" />
+
+                                </td>
+                                <td data-label='Action'>
+                                    {
+                                        user === 'admin' ? status === 'block' ? <span class="ussus">UnBlock</span> : <span class="sus">Block</span> : ''
+                                    }
+                                    <Link to='/dashborad/sub-admin/profile/55'>
+                                    Profile
+                                    </Link>
+                                </td>
+                               </tr>
+                               <tr>
+                               <td data-label='No'>1</td>
+                                <td data-label='Name'>Zihadul Islam</td>
+                                <td data-label='Email'>Zihadul1010@gmail.com</td>
+                                <td data-label='image' className='image'>
+                                    <img src="https://i.ibb.co/WpVNgJc/react-1.jpg" alt="" />
+
+                                </td>
+                                <td data-label='Action'>
+                                    {
+                                        user === 'admin' ? status === 'block' ? <span class="ussus">UnBlock</span> : <span class="sus">Block</span> : ''
+                                    }
+                                    <Link to='/dashborad/sub-admin/profile/55'>
+                                    Profile
+                                    </Link>
+                                </td>
+                               </tr>
+                               <tr>
+                               <td data-label='No'>1</td>
+                                <td data-label='Name'>Zihadul Islam</td>
+                                <td data-label='Email'>Zihadul1010@gmail.com</td>
+                                <td data-label='image' className='image'>
+                                    <img src="https://i.ibb.co/WpVNgJc/react-1.jpg" alt="" />
+
+                                </td>
+                                <td data-label='Action'>
+                                    {
+                                        user === 'admin' ? status === 'block' ? <span class="ussus">UnBlock</span> : <span class="sus">Block</span> : ''
+                                    }
+                                    <Link to='/dashborad/sub-admin/profile/55'>
+                                    Profile
+                                    </Link>
+                                </td>
+                               </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
